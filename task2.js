68f4bf079981440689952bb4b68d049c8463020e
darinamandarina <darinamandarina97@gmail.com>
2018-06-06 17:44:26 +0300

    completed task2

--- task2.js
+++ task2.js
@@ -1,3 +1,21 @@
 /* Task 2 */
 
-/* Put your code here */
+let str = process.argv[2];
+let n = Number(process.argv[3]);
+let len = str.length;
+let ch_arr = [];
+let encr_str =  '';
+
+const encryption = function(){
+	for(let i=0; i<len; i++){
+		ch_arr.push(str.charCodeAt(i));
+		if( str.match(/[a-zA-Z]/g)[i] != null) {
+			ch_arr[i]=ch_arr[i]+n;			
+		}
+		encr_str += String.fromCharCode(ch_arr[i]);	
+		
+	}
+	console.log(encr_str);	
+};
+
+encryption();
