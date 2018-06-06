cf02fc009b739f96f762c3c63381f1f69582d240
darinamandarina <darinamandarina97@gmail.com>
2018-06-06 18:24:33 +0300

    some changes

--- task2.js
+++ task2.js
@@ -1,19 +1,20 @@
 /* Task 2 */
 
-let str = process.argv[2];
-let n = Number(process.argv[3]);
-let len = str.length;
+let s = process.argv[2];
+let k = Number(process.argv[3]);
+let n = s.length;
 let ch_arr = [];
 let encr_str =  '';
 
 const encryption = function(){
-	for(let i=0; i<len; i++){
-		ch_arr.push(str.charCodeAt(i));
-		if( str.match(/[a-zA-Z]/g)[i] != null) {
-			ch_arr[i]=ch_arr[i]+n;			
+	for(let i=0; i<n; i++){
+		if (s.charCodeAt(i)<=127 && n>=1 && n<=10000 && k>=1 && k<=10000){
+			ch_arr.push(s.charCodeAt(i));
+			if( (s.match(/[a-zA-Z]/g)[i] != null)){
+				ch_arr[i]=ch_arr[i]+k;			
+			}
+			encr_str += String.fromCharCode(ch_arr[i]);	
 		}
-		encr_str += String.fromCharCode(ch_arr[i]);	
-		
 	}
 	console.log(encr_str);	
 };
