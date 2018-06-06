71cf6712cb99720decc7e7b4640eb08536ee5c93
darinamandarina <darinamandarina97@gmail.com>
2018-06-06 15:36:49 +0300

    completed task

--- task1.js
+++ task1.js
@@ -1,3 +1,16 @@
 /* Task 1 */
 
-/* Put your code here */
+let e = Number(process.argv[3]);
+let n = Number(process.argv[2]);
+let sum = 0;
+if (n >= 1 && e <= 100){
+	for (let i = 1; i<=n; i++){
+		let st = i;
+		for (let j = 1; j < e; j++){
+			st = st*i;	
+			}
+		sum = sum + st;	
+				
+	}
+}
+console.log(sum);
