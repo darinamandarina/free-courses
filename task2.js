49c3f3f98322d869510ffc11719025fef7cdee2d
darinamandarina <darinamandarina97@gmail.com>
2018-06-08 14:27:00 +0300

    many changes

--- task2.js
+++ task2.js
@@ -3,20 +3,22 @@
 let s = process.argv[2];
 let k = Number(process.argv[3]);
 let n = s.length;
-let ch_arr = [];
 let encr_str =  '';
-
-const encryption = function(){
-	for(let i=0; i<n; i++){
-		if (s.charCodeAt(i)<=127 && n>=1 && n<=10000 && k>=1 && k<=10000){
-			ch_arr.push(s.charCodeAt(i));
-			if( (s.match(/[a-zA-Z]/g)[i] != null)){
-				ch_arr[i]=ch_arr[i]+k;			
-			}
-			encr_str += String.fromCharCode(ch_arr[i]);	
-		}
-	}
-	console.log(encr_str);	
-};
-
+var alf = "abcdefghijklmnopqrstuvwxyz";
+let a_len = alf.length;
+function encryption(){
+    for (let i=0; i < n; i++)
+    {
+        let ind = alf.indexOf(s[i].toLowerCase());
+        if(ind != -1){
+          if (s[i].toLowerCase() == s[i])
+            encr_str += alf[(ind + k) % a_len];
+          else
+             encr_str += (alf[(ind + k) % a_len]).toUpperCase();
+        }        
+        else
+            encr_str += s[i];
+    }
+  console.log(encr_str);
+}
 encryption();
