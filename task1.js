b8873b29a7ed10608aafc5d1f1c5a2471ca17b85
Darina Pairel <darinamandarina@users.noreply.github.com>
2018-06-14 22:19:05 +0300

    updates

--- task1.js
+++ task1.js
@@ -5,11 +5,7 @@ let n = Number(process.argv[2]);
 let sum = 0;
 if (n >= 1 && e <= 100){
 	for (let i = 1; i<=n; i++){
-		let st = i;
-		for (let j = 1; j < e; j++){
-			st = st*i;	
-			}
-		sum = sum + st;	
+		sum += i**e;	
 				
 	}
 }
